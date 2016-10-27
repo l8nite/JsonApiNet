e688fcdb36ff18d6544936a368c35e1374011151
Prince Kamalanathan <prince.kamalanathan@rbi.co.uk>
2016-10-27 10:09:48 +0100

    Fixed bug when deserialising with null relations and made the deserialisation of relationships optional

--- JsonApiNet/JsonConverters/ResourceLinkageJsonConverter.cs
+++ JsonApiNet/JsonConverters/ResourceLinkageJsonConverter.cs
@@ -28,7 +28,7 @@ namespace JsonApiNet.JsonConverters
         {
             var token = JToken.Load(reader);
 
-            if (token == null || !token.HasValues)
+            if (token == null)
             {
                 return null;
             }
@@ -47,6 +47,9 @@ namespace JsonApiNet.JsonConverters
                         {
                             token.ToObject<JsonApiResourceIdentifier>()
                         };
+                    break;
+                case JTokenType.Null:
+                    linkage = null;
                     break;
                 default:
                     throw new JsonApiFormatException("Resource linkage was not null, an object, or an array");
